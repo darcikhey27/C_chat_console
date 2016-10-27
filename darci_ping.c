1691dc3693b435370c5343c116acefd280de2b68
darci khey <darcikhey27@gmail.com>
2016-10-26 22:04:39 -0700

    Added pre files

---
+++ darci_ping.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h> // str len
+#include<sys/socket.h> // socket
+#include<arpa/inet.h>  // ip address
+#include<unistd.h>     // ?
+
+int main(int argc, char *argv[])
+{
+    int socket_desc, client_sock, c, read_size;
+    
+
+
+}
---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("Enter your work here\n");
+	int num = 0;
+
+	scanf("%d", &num);
+
+	printf("your num is %d\n",num);
+
+	return 0;
+
+}
