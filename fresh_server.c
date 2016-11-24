c3ae823b62094f4097c94274f87f15dc106474e2
Darci Khey <darcikhey27@gmail.com>
2016-11-23 16:07:29 -0800

    basic client/server is working

---
+++ fresh_server.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+int main(int argc, char *argv[])
+{
+	int socket_dh;
+	struct sockaddr_in server;
+
+	// create socket
+	socket_dh = socket(AF_INET, SOCK_STREAM, 0);
+	printf("Sokcet dh is %d\n", socket_dh);	
+	
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons( 8888 );
+	
+	// bind
+	int stat;
+	stat = bind(socket_dh, (struct sockaddr *)&server, sizeof(server));
+	printf("bind status %d\n", stat);
+	if(stat < 0)
+		printf("bad bind status\n");
+
+	//listen for connections
+    listen(socket_dh, 2);
+
+    // accept incoming connections
+    puts("Wating for client");
+    int client_socket;
+    struct sockaddr_in client_addr;
+    
+    client_socket = accept(socket_dh, (struct sockaddr *)&client_addr,(socklen_t*)&client_socket); 
+    printf("client socket %d\n", client_socket); 
+    
+   // recieve the message and send somthing back
+   int count;
+   char recv_buf[1000];
+   count = recv(client_socket, recv_buf, 2000, 0);
+   printf("msg from client %s\n", recv_buf);
+	
+ 	
+
+	
+}// end main
